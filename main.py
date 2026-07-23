ef32f0dfe6705d9218d762caed72b8cd51a7b659
Luiza <luiza_silva2@estudante.sesisenai.org.br>
2026-07-23 20:45:42 -0300

    feat/ Começa o projeto

---
+++ main.py
@@ -0,0 +1,9 @@
+from models.funcionario import funcionario
+
+funcionario1 = funcionario
+("carlos ", "Silva", "11111111111", "operador " , "CNC" , 4500)
+
+print(funcionario1.nome)
+print(funcionario1.cpf)
+print(funcionario1.cargo)
+print(funcionario1.salario)
---
+++ models/funcionario.py
@@ -0,0 +1,6 @@
+class funcionario:
+        def init(self, nome, cpf, cargo, salario):
+            self.nome = nome
+            self.cpf = cpf
+            self.cargo = cargo
+            self.salario = salario
